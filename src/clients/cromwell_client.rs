2100aa3df1fbf2b5ceea02b4e0b55699bf748f4c
Kevin Lydon <klydon@broadinstitute.org>
2020-05-15 11:25:11 -0400

    Added test for running test on cromwell

--- src/clients/cromwell_client.rs
+++ src/clients/cromwell_client.rs
@@ -4,15 +4,12 @@
 //! response data
 
 use actix_multipart_rfc7578::client::multipart;
-use actix_web::client::{Client, ClientResponse, SendRequestError};
-use serde::{Deserialize, Serialize};
-use std::path::Path;
-use actix_codec::Decoder;
-use actix_web::http::StatusCode;
-use actix_web::web::Bytes;
-use actix_web::dev::{PayloadStream, Payload};
+use actix_web::client::{Client, SendRequestError};
+use serde::Deserialize;
+use std::path::PathBuf;
 use actix_web::error::PayloadError;
 use std::str::Utf8Error;
+use std::fmt;
 
 /// Configuration struct for sending requests to a Cromwell server
 ///
@@ -23,45 +20,60 @@ pub struct CromwellClient {
     cromwell_address: String,
 }
 
-pub struct JobData<'a> {
-    labels: Option<Box<Path>>,
-    workflow_dependencies: Option<Box<Path>>,
-    workflow_inputs: Option<Box<Path>>,
-    workflow_inputs2: Option<Box<Path>>,
-    workflow_inputs3: Option<Box<Path>>,
-    workflow_inputs4: Option<Box<Path>>,
-    workflow_inputs5: Option<Box<Path>>,
+pub struct JobData {
+    labels: Option<PathBuf>,
+    workflow_dependencies: Option<PathBuf>,
+    workflow_inputs: Option<PathBuf>,
+    workflow_inputs2: Option<PathBuf>,
+    workflow_inputs3: Option<PathBuf>,
+    workflow_inputs4: Option<PathBuf>,
+    workflow_inputs5: Option<PathBuf>,
     workflow_on_hold: Option<bool>,
-    workflow_options: Option<Box<Path>>,
+    workflow_options: Option<PathBuf>,
     workflow_root: Option<String>,
-    workflow_source: Option<Box<&'a Path>>,
+    workflow_source: Option<PathBuf>,
     workflow_type: Option<WorkflowTypeEnum>,
     workflow_type_version: Option<WorkflowTypeVersionEnum>,
     workflow_url: Option<String>,
 }
 
-#[derive(Serialize)]
 pub enum WorkflowTypeEnum {
     WDL,
     CWL
 }
 
-#[derive(Serialize)]
+impl fmt::Display for WorkflowTypeEnum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            WorkflowTypeEnum::WDL => write!(f, "{}", "WDL"),
+            WorkflowTypeEnum::CWL => write!(f, "{}", "CWL"),
+        }
+    }
+}
+
 pub enum WorkflowTypeVersionEnum {
-    #[serde(rename = "draft-2")]
     DraftDash2,
-    #[serde(rename = "1.0")]
     OnePointZero,
-    #[serde(rename = "v1.0")]
     VOnePointZero,
 }
 
-#[derive(Deserialize)]
+impl fmt::Display for WorkflowTypeVersionEnum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            WorkflowTypeVersionEnum::DraftDash2 => write!(f, "{}", "draft-2"),
+            WorkflowTypeVersionEnum::OnePointZero => write!(f, "{}", "1.0"),
+            WorkflowTypeVersionEnum::VOnePointZero => write!(f, "{}", "v1.0"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 pub struct WorkflowIdAndStatus {
     id: String,
     status: String,
 }
 
+#[derive(Debug)]
 pub enum StartJobError {
     Json(serde_json::error::Error),
     Io(std::io::Error),
@@ -114,7 +126,8 @@ impl CromwellClient {
         };
 
         // Make request
-        let response = self.client.post(format!("{}/api/workflows/v1", self.cromwell_address))
+        let response = self.client.post(format!("http://{}/api/workflows/v1", self.cromwell_address))
+            .content_type(form.content_type())
             .send_body(multipart::Body::from(form))
             .await;
 
@@ -190,10 +203,10 @@ impl CromwellClient {
             }
         }
         if let Some(value) = job_data.workflow_type {
-            form.add_text("workflowType", value);
+            form.add_text("workflowType", value.to_string());
         }
         if let Some(value) = job_data.workflow_type_version {
-            form.add_text("workflowTypeVersion", value);
+            form.add_text("workflowTypeVersion", value.to_string());
         }
         if let Some(value) = job_data.workflow_url {
             form.add_text("workflowUrl", value);
@@ -207,17 +220,17 @@ impl CromwellClient {
 #[cfg(test)]
 mod tests {
     use crate::unit_test_util;
-    use super::CromwellClient;
     use super::JobData;
-    use std::path::Path;
+    use log::error;
+    use std::path::PathBuf;
 
-    #[test]
-    fn test_start_job_simple() {
+    #[actix_rt::test]
+    async fn test_start_job_simple() {
         // Get client
         let client = unit_test_util::get_cromwell_client();
         // Create job data with simple test workflow
-        let test_path = Path::new("testdata/test_workflow.wdl");
-        let job_data = JobData{
+        let test_path = PathBuf::from("testdata/test_workflow.wdl");
+        let job_data = JobData {
             labels: None,
             workflow_dependencies: None,
             workflow_inputs: None,
@@ -228,7 +241,7 @@ mod tests {
             workflow_on_hold: None,
             workflow_options: None,
             workflow_root: None,
-            workflow_source: Some(Box::new(test_path)),
+            workflow_source: Some(test_path),
             workflow_type: None,
             workflow_type_version: None,
             workflow_url: None,
@@ -236,8 +249,10 @@ mod tests {
 
         let response = client.start_job(job_data).await;
 
+        println!("response: {:?}", response);
+
         assert_eq!(
-            response.unwrap().status,
+            response.ok().unwrap().status,
             String::from("Submitted")
         );
     }
